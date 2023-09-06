3cb8412cb9fbffdb37bd86d806a9970d19627803
MHF3 <mfagan2006@gmail.com>
2023-09-05 20:36:57 -0400

    Made a little progress
    - Hopefully found the way to access the needed information from the correct document
    - Need to figure out how to correctly add the new data window to scouted matches and call the new MatchData correctly with the required parameters, then test

--- app/src/main/java/org/wildstang/wildrank/androidv2/views/data/charged_up/MatchDataAutoTopCones.java
+++ app/src/main/java/org/wildstang/wildrank/androidv2/views/data/charged_up/MatchDataAutoTopCones.java
@@ -3,17 +3,30 @@ package org.wildstang.wildrank.androidv2.views.data.charged_up;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Document;
 
+import org.wildstang.wildrank.androidv2.data.DatabaseManager;
 import org.wildstang.wildrank.androidv2.interfaces.IMatchDataView;
 import org.wildstang.wildrank.androidv2.views.data.MatchDataView;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class MatchDataAutoTopCones extends MatchDataView implements IMatchDataView {
+
     public MatchDataAutoTopCones(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    private String matchKey;
+    private String teamKey;
+
+    public MatchDataAutoTopCones(Context context, AttributeSet attrs, String matchKey, String teamKey) {
+        super(context, attrs);
+        this.matchKey = matchKey;
+        this.teamKey = teamKey;
     }
 
     @Override
@@ -24,15 +37,21 @@ public class MatchDataAutoTopCones extends MatchDataView implements IMatchDataVi
             return;
         }
         boolean didSomething = false;               // catch teams that did nothing -> present a "N/A"
-        int topCones = 0;
-        for (Document document : documents) {
-            Map<String, Object> data = (Map<String, Object>) document.getProperty("data");
-            if (data == null) {
-                return;
-            }
-            topCones = (int) data.get("auto_top_cones");
-            didSomething = true;
+        int autoTopCones = 0;
+        Document doc;
+        try {
+            doc = DatabaseManager.getInstance(this.getContext()).getMatchResults(matchKey, teamKey);
+        } catch (CouchbaseLiteException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        setValueText(formatNumberAsString(topCones), "gray");
+        Map<String, Object> data = (Map<String, Object>) doc.getProperty("data");
+        if (data == null) {
+            return;
+        }
+        autoTopCones = (int) data.get("auto_top_cones");
+        didSomething = true;
+        setValueText("" + formatNumberAsString(autoTopCones), "gray");
     }
 }
