8f9f4d90a17b62ac1bdf0373b313be21425784e2
MHF3 <mfagan2006@gmail.com>
2024-01-30 20:58:48 -0500

    Added in buttons for map, added in logic for displaying red vs blue, for some reason logic doesn't seem to work

--- app/src/main/java/org/wildstang/wildrank/androidv2/fragments/AutonomousScoutingFragment.java
+++ app/src/main/java/org/wildstang/wildrank/androidv2/fragments/AutonomousScoutingFragment.java
@@ -1,9 +1,13 @@
 package org.wildstang.wildrank.androidv2.fragments;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.wildstang.wildrank.androidv2.R;
 
@@ -12,6 +16,34 @@ public class AutonomousScoutingFragment extends ScoutingFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scout_autonomous, container, false);
+        String assignedTeamType = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("assignedTeams", "red_1");
+        if (assignedTeamType.contains("red")) {
+            ((ImageView) view.findViewById(R.id.redautonmap)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton1)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton2)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton3)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton4)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton5)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton6)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton7)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton8)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton9)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.redbutton10)).setVisibility(View.VISIBLE);
+            ((TextView) view.findViewById(R.id.redlabel)).setVisibility(View.VISIBLE);
+        } else {
+            ((ImageView) view.findViewById(R.id.blueautonmap)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton1)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton2)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton3)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton4)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton5)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton6)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton7)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton8)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton9)).setVisibility(View.VISIBLE);
+            ((Button) view.findViewById(R.id.bluebutton10)).setVisibility(View.VISIBLE);
+            ((TextView) view.findViewById(R.id.bluelabel)).setVisibility(View.VISIBLE);
+        }
         return view;
     }
 }
