941816592695315b8fb842191696759766eec533
MHF3 <mfagan2006@gmail.com>
2024-01-31 21:01:58 -0500

    Added checkbox to auto screen, made custom button and fixed buttons for auto screen, hopefully custom buttons will keep track of time when pressed

---
+++ app/src/main/java/org/wildstang/wildrank/androidv2/views/scouting/ScoutingButtonView.java
@@ -0,0 +1,60 @@
+package org.wildstang.wildrank.androidv2.views.scouting;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.Button;
+
+import org.wildstang.wildrank.androidv2.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Map;
+
+public class ScoutingButtonView extends ScoutingView {
+    private Button buttonView;
+
+    private OnClickListener listener;
+
+    private ArrayList clicks;
+
+    public ScoutingButtonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        inflater.inflate(R.layout.custom_view_button, this, true);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ScoutingView, 0, 0);
+        String label = a.getString(R.styleable.ScoutingView_label);
+        a.recycle();
+
+        buttonView = (Button) findViewById(R.id.button);
+        buttonView.setClickable(true);
+        // This conflicts with our custom state saving
+        buttonView.setSaveEnabled(false);
+
+        clicks = new ArrayList();
+
+        this.setOnClickListener(v -> {
+            clicks.add(Calendar.getInstance().getTimeInMillis());
+        });
+    }
+
+    @Override
+    public void writeContentsToMap(Map<String, Object> map) {
+        map.put(key, clicks);
+    }
+
+    public void setClicks(ArrayList list) {
+        clicks = list;
+    }
+
+    @Override
+    public void restoreFromMap(Map<String, Object> map) {
+        Object list = map.get(key);
+        if (list instanceof ArrayList) {
+            setClicks((ArrayList) list);
+        }
+    }
+}
