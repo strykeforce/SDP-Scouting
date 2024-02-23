93f6318190b35b8e11467a03734ead41c92745c7
MHF3 <mfagan2006@gmail.com>
2024-02-22 20:54:33 -0500

    Removed redundancies, slight improving of auton and teleop screens

--- app/src/main/java/org/wildstang/wildrank/androidv2/fragments/AutonomousScoutingFragment.java
+++ app/src/main/java/org/wildstang/wildrank/androidv2/fragments/AutonomousScoutingFragment.java
@@ -20,32 +20,32 @@ public class AutonomousScoutingFragment extends ScoutingFragment {
         String assignedTeamType = PreferenceManager.getDefaultSharedPreferences(getActivity()).getString("assignedTeam", "red_1");
         if (assignedTeamType.contains("red")) {
             System.out.println("Team is red");
-            ((ImageView) view.findViewById(R.id.redautonmap)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton1)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton2)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton3)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton4)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton5)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton6)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton7)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton8)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton9)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.redbutton10)).setVisibility(View.VISIBLE);
-            ((TextView) view.findViewById(R.id.redlabel)).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redautonmap).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton1).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton2).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton3).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton4).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton5).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton6).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton7).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton8).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton9).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redbutton10).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.redlabel).setVisibility(View.VISIBLE);
         } else {
             System.out.println("Team is blue");
-            ((ImageView) view.findViewById(R.id.blueautonmap)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton1)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton2)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton3)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton4)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton5)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton6)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton7)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton8)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton9)).setVisibility(View.VISIBLE);
-            ((ScoutingButtonView) view.findViewById(R.id.bluebutton10)).setVisibility(View.VISIBLE);
-            ((TextView) view.findViewById(R.id.bluelabel)).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.blueautonmap).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton1).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton2).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton3).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton4).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton5).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton6).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton7).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton8).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton9).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluebutton10).setVisibility(View.VISIBLE);
+            view.findViewById(R.id.bluelabel).setVisibility(View.VISIBLE);
         }
         return view;
     }
