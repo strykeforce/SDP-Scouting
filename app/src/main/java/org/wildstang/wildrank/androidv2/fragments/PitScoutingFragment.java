3fd9432d931a9894d24772b2645d107069ded36f
MHF3 <mfagan2006@gmail.com>
2025-02-18 20:07:15 -0500

    Camera integration seems to be working (file is named properly, shows up in wildrank folder, and is accessed to display for robot photo in match scouting)

--- app/src/main/java/org/wildstang/wildrank/androidv2/fragments/PitScoutingFragment.java
+++ app/src/main/java/org/wildstang/wildrank/androidv2/fragments/PitScoutingFragment.java
@@ -1,34 +1,31 @@
 package org.wildstang.wildrank.androidv2.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 
 
 import org.wildstang.wildrank.androidv2.R;
+import org.wildstang.wildrank.androidv2.Utilities;
 import org.wildstang.wildrank.androidv2.activities.ScoutPitActivity;
 
 public class PitScoutingFragment extends ScoutingFragment implements View.OnClickListener {
 
     Button Camera_Button;
-    ImageView imageView;
-    int Teamkey = 2767;
+    int teamKey;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scout_pit, container, false);
         view.findViewById(R.id.finish).setOnClickListener(this);
 
+        teamKey = Utilities.teamNumberFromTeamKey(((ScoutPitActivity) getActivity()).teamKey);
         Camera_Button = (Button) view.findViewById(R.id.CameraButton);
         Camera_Button.setOnClickListener(this);
+        
         return view;
-
-
-
     }
 
     @Override
@@ -38,19 +35,11 @@ public class PitScoutingFragment extends ScoutingFragment implements View.OnClic
             ((ScoutPitActivity) getActivity()).finishScouting();
         } else if (id== R.id.CameraButton) {
             System.out.println("Launching Camera");
-            ScoutingCamera.launchCamera(getActivity(),Teamkey);
+            ScoutingCamera.launchCamera(getActivity(), teamKey);
 
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (ScoutingCamera.isCameraResult(requestCode) && resultCode == getActivity().RESULT_OK) {
-            System.out.println("IT ACTUALLY WORKEDDDDDDDDDDD");
-        }
-
-    }
     @Override
     public void onResume() {
         super.onResume();
